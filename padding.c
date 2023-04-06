28dd99ec076e5005cc8403e4ad71b4860195745a
voidgrace <118725789+voidgrace@users.noreply.github.com>
2023-04-06 22:59:18 +0900

    Update padding.c

--- padding.c
+++ padding.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 //가장큰 자료형인 int의 크기 4byte를 기준으로 할당
 struct student {
-    char lastName[13]; /* 13 bytes */   //이름
-    int studentId; /* 4 bytes */        //학번
-    short grade; /* 2 bytes */          //학년
+    char lastName[13]; /* 13 bytes */   //문자열 : 이름
+    int studentId; /* 4 bytes */        //정수형 변수 : 학번
+    short grade; /* 2 bytes */          //정수형 변수 : 학년
     };
 int main()
 {
     printf("[-----[강은혜][2022041047]-----]\n");
-    struct student pst;
+    struct student pst;                 //구조체 변수 선언
     
     printf("size of student = %ld\n", sizeof(struct student));  // padding: 19byte가 아닌 배수가 되는 16+4+4=24byte로 할당
 
