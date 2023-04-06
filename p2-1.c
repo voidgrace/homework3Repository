49dfb248c50b1d62bfd023cb22167022cf442653
Eunhye Kang <gp7981@naver.com>
2023-04-06 22:24:01 +0900

    누락된 주석 수정

--- p2-1.c
+++ p2-1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define MAX_SIZE 100 //최대값 == 100
-float sum1(float list[], int);     //함수 주소
-float sum2(float *list, int);       //함수 포인터
+float sum1(float list[], int);     //배열 전달
+float sum2(float *list, int);       //포인터로 배열전달
 float sum3(int n, float *list);      //인자호출 순서에 따른 주소 할당
 float input[MAX_SIZE], answer;      
 int i;                              
@@ -13,60 +13,66 @@ void main(void)
     /* for checking call by reference */
     printf("[-----[강은혜][2022041047]-----]\n");
     
-    //
+    //평범한 배열 인자 전달
     printf("--------------------------------------\n"); 
     printf(" sum1(input, MAX_SIZE) \n");                    
     printf("--------------------------------------\n");
     printf("input \t= %p\n", input);
-    answer = sum1(input, MAX_SIZE);  
+    answer = sum1(input, MAX_SIZE);                     
+    printf("The sum is: %f\n\n", answer);              
 
+    //배열, 정수 순으로 인자 전달
     printf("The sum is: %f\n\n", answer);               
     printf("--------------------------------------\n");
     printf(" sum2(input, MAX_SIZE) \n");                
     printf("--------------------------------------\n");
     printf("input \t= %p\n", input);                    
-    answer = sum2(input, MAX_SIZE);                     //
+    answer = sum2(input, MAX_SIZE);                     
+    printf("The sum is: %f\n\n", answer);              
 
-    printf("The sum is: %f\n\n", answer);               //
+    // 정수, 배열 순으로 인자 전달
+    printf("The sum is: %f\n\n", answer);               
     printf("--------------------------------------\n");
     printf(" sum3(MAX_SIZE, input) \n");            
     printf("--------------------------------------\n");
     printf("input \t= %p\n", input);                    
-    answer = sum3(MAX_SIZE, input);                     //
 
-    printf("The sum is: %f\n\n", answer);               //
+    answer = sum3(MAX_SIZE, input);                     
+    printf("The sum is: %f\n\n", answer);              
 }
-float sum1(float list[], int n) 
+
+float sum1(float list[], int n) //list의 합을 반환하는 함수
 {
-    printf("list\t= %p\n", list);                       //
-    printf("&list\t= %p\n\n", &list);                   //
+    printf("list\t= %p\n", list); //list의 원래 배열 input의 주소                    
+    printf("&list\t= %p\n\n", &list);//sum1함수의 지역변수 list의 주소 
     
-    int i;                                              //
-    float tempsum = 0;                                  //
+    int i;                                              
+    float tempsum = 0;                                  
     for(i = 0;i < n;i++)                              
-    tempsum += list[i];                                 //
+    tempsum += list[i];                                 
     
-    return tempsum;                                     //
+    return tempsum;  //배열의 합 반환
     }
-float sum2(float *list, int n){
-    printf("list\t= %p\n", list);                       
-    printf("&list\t= %p\n\n", &list);                   
+float sum2(float *list, int n)//배열과 정수 순으로 인자 받음
+{
+    printf("list\t= %p\n", list); //list의 원래 배열 input의 주소                      
+    printf("&list\t= %p\n\n", &list);//sum2함수의 지역변수 list의 주소                    
     
     int i;                                              
     float tempsum = 0;                                  
     for(i = 0;i < n;i++)                                
-    tempsum += *(list +i);                              //
+    tempsum += *(list +i);                              
     
-    return tempsum;                                     //
+    return tempsum;  //배열의 합 반환                                   
     }
     /* stack variable reuse test */
-float sum3(int n, float *list)                          //
+float sum3(int n, float *list)//     
 {
-    printf("list\t= %p\n", list);                       //
-    printf("&list\t= %p\n\n", &list);                   //
-    int i;                                              //
-    float tempsum = 0;                                  //
+    printf("list\t= %p\n", list);  //list의 원래 배열 input의 주소                      
+    printf("&list\t= %p\n\n", &list);//sum3함수의 지역변수 list의 주소  //n이 더 먼저 선언되어, sum2와 다름
+    int i;                                              
+    float tempsum = 0;                                  
     for(i = 0;i < n;i++)        
-    tempsum += *(list +i);                              //
-    return tempsum;
+    tempsum += *(list +i);                              
+    return tempsum;//배열의 합 반환
     }
